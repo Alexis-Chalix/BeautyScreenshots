5f3c9ce602ca66bbc44da2175dbfb2be392db3a7
GitHub Action <action@github.com>
2022-11-04 17:25:48 +0000

    Template cleanup

--- src/main/kotlin/com/github/alexischalix/beautyscreenshots/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/alexischalix/beautyscreenshots/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.alexischalix.beautyscreenshots.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.alexischalix.beautyscreenshots.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/alexischalix/beautyscreenshots/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/alexischalix/beautyscreenshots/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.alexischalix.beautyscreenshots.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.alexischalix.beautyscreenshots.MyBundle
 
 class MyApplicationService {
 
